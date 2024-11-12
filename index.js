7f713f283a0012de1a2b5a73c324989804af79ae
Marvideo2009 <marvideomc.pro@gmail.com>
2024-11-12 19:01:23 +0100

    Update Style

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ async function fetchYear() {
 
         const commit = data.commit;
         const date = commit.author.date
-        console.log(date)
         document.getElementById("year").innerText = new Date(date).getFullYear()
     } catch (error) {
         document.getElementById("year").innerText =  new Date().getFullYear()
