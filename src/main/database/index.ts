4252c26d99ab941dabf735879e71e198a9add2a1
Marvideo2009 <marvideomc.pro@gmail.com>
2024-11-19 18:46:54 +0100

    adding all function db

--- src/main/database/index.ts
+++ src/main/database/index.ts
@@ -1,196 +1,227 @@
-import * as schema from "./schema"
+// import * as schema from "./schema"
 import path from "node:path"
 import { app } from "electron"
-import { drizzle } from "drizzle-orm/libsql"
-import { eq } from "drizzle-orm"
-import { pathExists, mkdir, createFile, readJSON } from "fs-extra"
+// import Database from 'libsql';
+import { pathExistsSync, createFileSync, mkdirSync, readJSONSync, writeFileSync } from "fs-extra"
+
+import { DatabaseType, Boards, BoardsUpdate, NewBoards, Collection, CollectionUpdate, NewCollection, Options, OptionsUpdate, NewOptions, Tasks, TaskUpdate, NewTask } from './types.ts' // this is the Database interface we defined earlier
+import Database from 'better-sqlite3'
+import { Kysely, SqliteDialect } from 'kysely'
 
 type DataSourceJson = {
     path: string
 }
 
 const appBasePath = path.join(app.getAppPath(), `${app.getName()}_Data`)
-if(!(await pathExists(appBasePath))) {
-    await mkdir(appBasePath)
+if(!(pathExistsSync(appBasePath))) {
+    mkdirSync(appBasePath)
 }
 
-const dataSourcePath = path.join(appBasePath, "data_source.json")
-if(!(await pathExists(dataSourcePath))) {
-    await createFile(appBasePath)
+let dataSourcePath = path.join(appBasePath, "data_source.json")
+if(!(pathExistsSync(dataSourcePath))) {
+    const baseConfig: DataSourceJson = { path: "" }
+    writeFileSync(dataSourcePath, JSON.stringify(baseConfig, null, 2))
 }
 
-const dataSource = await readJSON(dataSourcePath) as DataSourceJson
-const databaseURL = path.join(dataSource?.path ?? appBasePath, "local.db")
-
-const db = drizzle(databaseURL, { schema })
-
-type Options = typeof schema.optionsTable.$inferInsert
+const dbSourcePath = path.join(appBasePath, "local.db")
+if(!(pathExistsSync(dbSourcePath))) {
+    createFileSync(dbSourcePath)
+}
+const dataSource = readJSONSync(dataSourcePath) as DataSourceJson
+let databaseURL: string;
+if (dataSource?.path !== '') {
+  console.log("aaa")
+    databaseURL = path.join(dataSource?.path , "local.db")
+} else {
+    databaseURL = path.join(appBasePath.replace("C:", "file:"), "local.db")
+}
+console.log(databaseURL)
+console.log(appBasePath)
+const dialect = new SqliteDialect({
+    database: new Database(databaseURL),
+  })
+  
+  export const db = new Kysely<DatabaseType>({
+    dialect,
+  })
 
 export const addOption = async (option: Options) => {
-    return await db.insert(schema.optionsTable).values(option)
+  return await db.insertInto('options')
+    .values(option)
+    // const request = db.prepare('INSERT INTO options (key, value) VALUES (?, ?)')
+    // return request.get(option.key, option.value)
 }
 
 export const getOption = async (key: string) => {
-    return await db.select({ key: schema.optionsTable.key, value: schema.optionsTable.value })
-        .from(schema.optionsTable)
-        .where(eq(schema.optionsTable.key, key))
-        .get()
+  return await db.selectFrom('options')
+    .where('key', '=', key)
+    // const request = db.prepare('SELECT * FROM options WHERE key = ?')
+    // return request.get(key)
 }
 
-export const setOption = async (key: string, value: any ) => {
-    return await db.update(schema.optionsTable)
-        .set({ value: value.toString() })
-        .where(eq(schema.optionsTable.key, key))
-        .get()
+export const setOption = async (option: Options) => {
+  return await db.updateTable('options').set(option).where('id', '=', option.id)
+    // const request = db.prepare('UPDATE options set value = ? WHERE key = ?')
+    // return request.get(option.value, option.key)
 }
 
 export const removeOption = async (key: string ) => {
-    return await db.delete(schema.optionsTable)
-        .where(eq(schema.optionsTable.key, key))[0]
+  return await db.deleteFrom('options').where('key', '=', key)
+    // const request = db.prepare('DELETE FROM options WHERE key = ?')
+    // return request.get(key)
 }
 
 export const getPreferredPositionX = async () => {
-    return getOption("preferedPositionX")
+    return getOption("preferedPositionX")[0].value
 }
 
 export const setPreferredPositionX = async (newPreferredPositionX: number ) => {
-    return setOption("preferredPositionX", newPreferredPositionX)
+    return setOption({ id: getOption("preferedPositionX")[0].id, key:"preferredPositionX", value: newPreferredPositionX.toString() })
 }
 
 export const getPreferredPositionY = async () => {
-    return getOption("preferredPositionY")
+    return getOption("preferredPositionY")[0].value
 }
 
 export const setPreferredPositionY = async (newPreferredPositionY: number ) => {
-    return setOption("preferredPositionY", newPreferredPositionY)
+    return setOption({ id: getOption("preferredPositionY")[0].id, key: "preferredPositionY", value: newPreferredPositionY.toString() })
 }
 
 export const getPreferredWidthResolution = async () => {
-    return getOption("preferredWidthResolution")
+    return getOption("preferredWidthResolution")[0].value
 }
 
 export const setPreferredWidthResolution = async (newPreferredWidthResolution: number ) => {
-    return setOption("preferredWidthResolution", newPreferredWidthResolution)
+    return setOption({ id: getOption("preferredWidthResolution")[0].id, key: "preferredWidthResolution", value: newPreferredWidthResolution.toString() })
 }
 
 export const getPreferredHeightResolution = async () => {
-    return getOption("preferredHeight")
+    return getOption("preferredHeight")[0].value
 }
 
 export const setPreferredHeightResolution = async (newPreferredHeightResolution: number ) => {
-    return setOption("preferredHeightResolution", newPreferredHeightResolution)
+    return setOption({ id: getOption("preferredHeight")[0].id, key: "preferredHeightResolution", value: newPreferredHeightResolution.toString() })
 }
 
 export const getBlur = async () => {
-    return getOption("blur")
+    return getOption("blur")[0].value
 }
 
 export const setBlur = async (blur: number ) => {
-    return setOption("blur", blur)
+    return setOption({ id: getOption("blur")[0].id, key: "blur", value: blur.toString() })
 }
 
 export const getTheme = async () => {
-    return getOption("theme")
+    return getOption("theme")[0].value
 }
 
 export const setTheme = async (theme: number ) => {
-    return setOption("theme", theme)
+    return setOption({ id: getOption("theme")[0].id, key: "theme", value: theme.toString() })
 }
 
 // Board
 
-type Board = typeof schema.boardsTable.$inferInsert
-
 export const fetchOneBoard = async (id: number) => {
-    return await db.select()
-        .from(schema.boardsTable)
-        .where(eq(schema.boardsTable.id, id))[0]
+  return await db.selectFrom('boards').where('id', '=', id)[0]
+    // const request = db.prepare(`SELECT * FROM boards WHERE id = ?`)
+    // return request.get(id)
 }
 
 export const fetchAllBoard = async () => {
-    return await db.select()
-        .from(schema.boardsTable)
+  return await db.selectFrom('boards').selectAll()
+    // return db.exec(`SELECT * FROM boards`)
 }
 
-export const insertBoard = async (board: Board) => {
-    return await db.insert(schema.boardsTable).values(board)
+export const insertBoard = async (board: Boards) => {
+  return await db.insertInto('boards').values(board)
+    // const request = db.prepare('INSERT INTO boards (name) VALUES (?)')
+    // return request.get(board.name)
 }
 
-export const updateBoard = async (board: Board) => {
-    return await db.update(schema.tasksTable)
-        .set(board)
-        .where(eq(schema.tasksTable, board.id))
+export const updateBoard = async (board: Boards) => {
+  return db.updateTable('boards').set(board).where('id', '=', board.id)
+    // const request = db.prepare('UPDATE boards set name = ? WHERE id = ?')
+    // return request.get(board.name, board.id)
 }
 
 export const deleteBoard = async (boardId: number) => {
-    return await db.delete(schema.tasksTable).where(eq(schema.tasksTable.id, boardId))
+  return db.deleteFrom('boards').where('id', '=', boardId)
+    // const request = db.prepare('DELETE FROM boards WHERE id = ?')
+    // return request.get(boardId)
 }
 
 // Collection
 
-type Collection = typeof schema.collectionsTable.$inferInsert
-
 export const fetchOneCollection = async (id: number) => {
-    return await db.select()
-        .from(schema.collectionsTable)
-        .where(eq(schema.collectionsTable.id, id))[0]
+  return await db.selectFrom('collections').where('id', '=', id)[0]
+    // return await db.select()
+    //     .from(schema.collectionsTable)
+    //     .where(eq(schema.collectionsTable.id, id))[0]
 }
 
-export const fetchAllCollection = async (boardId: number) => {
-    return await db.select()
-        .from(schema.collectionsTable)
-        .where(eq(schema.collectionsTable.boardId, boardId))
+export const fetchAllCollection = async () => {
+  return await db.selectFrom('collections').selectAll()
+    // return await db.select()
+    //     .from(schema.collectionsTable)
+    //     .where(eq(schema.collectionsTable.boardId, boardId))
 }
 
 export const insertCollection = async (collection: Collection) => {
-    return await db.insert(schema.collectionsTable).values(collection)
+  return await db.insertInto('collections').values(collection)
+    // return await db.insert(schema.collectionsTable).values(collection)
 }
 
 export const updateCollection = async (collection: Collection) => {
-    return await db.update(schema.collectionsTable)
-        .set(collection)
-        .where(eq(schema.collectionsTable, collection.id))
+  return db.updateTable('collections').set(collection).where('id', '=', collection.id)
+    // return await db.update(schema.collectionsTable)
+    //     .set(collection)
+    //     .where(eq(schema.collectionsTable, collection.id))
 }
 
 export const deleteCollection = async (collectionId: number) => {
-    return await db.delete(schema.collectionsTable).where(eq(schema.collectionsTable.id, collectionId))
+  return db.deleteFrom('collections').where('id', '=', collectionId)
+    // return await db.delete(schema.collectionsTable).where(eq(schema.collectionsTable.id, collectionId))
 }
 
 // Tasks
 
-type Task = typeof schema.tasksTable.$inferInsert
-
 export const fetchOneTask = async (id: number) => {
-    return await db.select()
-        .from(schema.tasksTable)
-        .where(eq(schema.tasksTable.id, id))[0]
+  return await db.selectFrom('tasks').where('id', '=', id)[0]
+    // return await db.select()
+    //     .from(schema.tasksTable)
+    //     .where(eq(schema.tasksTable.id, id))[0]
 }
 
 export const fetchAllTasks = async (listId: number) => {
-    return await db.select()
-        .from(schema.tasksTable)
-        .where(eq(schema.tasksTable.collectionId, listId))
+  return await db.selectFrom('tasks').selectAll()
+    // return await db.select()
+    //     .from(schema.tasksTable)
+    //     .where(eq(schema.tasksTable.collectionId, listId))
 }
 
-export const insertTask = async (task: Task) => {
-    return await db.insert(schema.tasksTable).values(task)
+export const insertTask = async (task: Tasks) => {
+  return await db.insertInto('tasks').values(task)
+    // return await db.insert(schema.tasksTable).values(task)
 }
 
-export const updateTask = async (task: Task) => {
-    return await db.update(schema.tasksTable)
-        .set(task)
-        .where(eq(schema.tasksTable, task.id))
+export const updateTask = async (task: Tasks) => {
+  return db.updateTable('tasks').set(task).where('id', '=', task.id)
+    // return await db.update(schema.tasksTable)
+    //     .set(task)
+    //     .where(eq(schema.tasksTable, task.id))
 }
 
 export const deleteTask = async (taskId: number) => {
-    return await db.delete(schema.tasksTable).where(eq(schema.tasksTable.id, taskId))
+  return db.deleteFrom('tasks').where('id', '=', taskId)
+    // return await db.delete(schema.tasksTable).where(eq(schema.tasksTable.id, taskId))
 }
 
 // Update Task Position
 
 export const updatePosition = async (taskId: number, newCollectionId: number, newPosition: number) => {
-    let task: Task = { id: taskId, collectionId: newCollectionId, order: newPosition }
-    return await db.update(schema.tasksTable)
-        .set(task)
-        .where(eq(schema.tasksTable, task.id))
+  return db.updateTable('tasks').set({ id: taskId, collectionId: newCollectionId.toString(), order: newPosition.toString() }).where('id', '=', taskId)
+    // let task: Task = { id: taskId, collectionId: newCollectionId, order: newPosition }
+    // return await db.update(schema.tasksTable)
+    //     .set(task)
+    //     .where(eq(schema.tasksTable, task.id))
 }
---
+++ src/main/database/types.ts
@@ -0,0 +1,65 @@
+import {
+    ColumnType,
+    Generated,
+    Insertable,
+    JSONColumnType,
+    Selectable,
+    Updateable,
+  } from 'kysely'
+  
+  export interface DatabaseType {
+    options: optionsTable
+    boards: boardsTable
+    collections: collectionsTable
+    tasks: tasksTable
+  }
+
+  export interface boardsTable {
+    id: Generated<number>
+    name: string
+  }
+
+  export type Boards = Selectable<boardsTable>
+  export type NewBoards = Insertable<boardsTable>
+  export type BoardsUpdate = Updateable<boardsTable>
+
+
+  export interface collectionsTable {
+    id: Generated<number>
+    boardsId: string
+    name: string
+    color: string
+  }
+
+  export type Collection = Selectable<collectionsTable>
+  export type NewCollection = Insertable<collectionsTable>
+  export type CollectionUpdate = Updateable<collectionsTable>
+
+
+  export interface optionsTable {
+    id: Generated<number>
+    key: string
+    value: string
+  }
+
+  export type Options = Selectable<optionsTable>
+  export type NewOptions = Insertable<optionsTable>
+  export type OptionsUpdate = Updateable<optionsTable>
+
+
+  export interface tasksTable {
+    id: Generated<number>
+    collectionId: string
+    order: string
+    name: string
+    description: string
+    dueDate: string
+  }
+
+  export type Tasks = Selectable<tasksTable>
+  export type NewTask = Insertable<tasksTable>
+  export type TaskUpdate = Updateable<tasksTable>
+  
+  // export type Person = Selectable<PersonTable>
+  // export type NewPerson = Insertable<PersonTable>
+  // export type PersonUpdate = Updateable<PersonTable>
