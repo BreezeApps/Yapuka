267460f981907beba2365f52dbc44ba184011123
Sn0wcode <dem9ngemarcel@laposte.net>
2025-09-02 10:45:58 +0200

    Update dependencies and remove updater plugin
    - Updated various Tauri plugins to their latest versions in Cargo.toml.
    - Removed updater capabilities from default.json and desktop.json.
    - Removed updater plugin initialization from lib.rs.
    - Cleaned up tauri.conf.json by removing updater configuration.
    - Commented out the checkForAppUpdates function in checkForUpdate.ts.
    - Added snapcraft.yaml for packaging the application as a Snap.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,7 +11,6 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
